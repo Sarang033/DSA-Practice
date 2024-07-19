a74fdc73e17c7184f962f130fd7c8c2bde92be13
SARANG SHARMA <89595882+Sarang033@users.noreply.github.com>
2024-07-19 21:59:41 +0530

    [767] [Time Beats: 31.99%] [Memory Beats: 53.45%] - LeetPush

---
+++ Heap(PriorityQueue)/767-Reorganize-String.java
@@ -0,0 +1,30 @@
+class Solution {
+    public String reorganizeString(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        for(char c: s.toCharArray()){
+            map.put(c, map.getOrDefault(c,0) + 1);
+        }
+        PriorityQueue<Character> pq = new PriorityQueue<>((a,b) -> map.get(b) - map.get(a));
+        pq.addAll(map.keySet());
+
+        StringBuilder sb = new StringBuilder();
+        while(pq.size() >= 2){
+            char ch1 = pq.poll();
+            char ch2 = pq.poll();
+            sb.append(ch1);
+            sb.append(ch2);
+
+            map.put(ch1, map.get(ch1) - 1);
+            map.put(ch2, map.get(ch2) - 1);
+
+            if(map.get(ch1) > 0) pq.add(ch1);
+            if(map.get(ch2) > 0) pq.add(ch2);
+        }
+        if(!pq.isEmpty()){
+            char ch =pq.poll();
+            if(map.get(ch) > 1) return "";
+            sb.append(ch);
+        }
+        return sb.toString();
+    }
+}
