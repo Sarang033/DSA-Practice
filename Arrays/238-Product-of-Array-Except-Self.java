551b9fde261bce7e035949933d0258eebe5f2a69
SARANG SHARMA <89595882+Sarang033@users.noreply.github.com>
2024-07-30 13:24:29 +0530

    [238] [Time Beats: 86.35%] [Memory Beats: 52.53%] - LeetPush

---
+++ Arrays/238-Product-of-Array-Except-Self.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int ans[]= new int[nums.length];
+        int prefix=1, postfix=1;
+        for(int i=0;i<nums.length;i++){
+            ans[i]=prefix;
+            prefix*=nums[i];
+        }
+        for(int j=nums.length-1;j>=0;j--){
+            ans[j]*=postfix;
+            postfix*=nums[j];
+        }
+        return ans;
+    }
+}
