95b19cea1d1eaefa4d346af15c9120dd5a432cb6
SARANG SHARMA <89595882+Sarang033@users.noreply.github.com>
2024-08-05 09:36:42 +0530

    [82] [Time Beats: 100.00%] [Memory Beats: 51.16%] - LeetPush

---
+++ LinkedList/82-Remove-Duplicates-from-Sorted-List-II.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode curr = temp;
+        while(curr.next != null && curr.next.next != null){
+            if(curr.next.val == curr.next.next.val) {
+                int duplicate = curr.next.val;
+                while(curr.next !=null && curr.next.val == duplicate) {
+                    curr.next = curr.next.next;
+                }
+            }
+            else{
+                curr = curr.next;
+            }
+        }
+        return temp.next;  
+    }
+}
